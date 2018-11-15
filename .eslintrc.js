4219ec166d4ba409131c1b2746a811700504e401
Trond Bergquist <trond_bergquist@hotmail.com>
2018-11-15 12:02:41 +0100

    Improve eslint-setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,17 @@ const ERROR = 2;
 module.exports = {
   "extends": ["@callstack"],
   "plugins": [
-    "prettier"
+    "prettier",
+    "react-native"
   ],
   "rules": {
+    "lines-between-class-members": [ERROR, "always", { exceptAfterSingleLine: true }],
+    "no-lonely-if": ERROR,
+    "no-else-return": ERROR,
+    "prefer-const": ERROR,
+    "no-duplicate-imports": ERROR,
+    "curly": ERROR,
+    "react-native/no-unused-styles": ERROR,
     "react/destructuring-assignment": ERROR,
     "prettier/prettier": ERROR,
     "import/no-extraneous-dependencies": [
