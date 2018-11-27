b72e080843bedf9586585684dcbad988e9b32d56
lukewalczak <lukasz.walczak.pwr@gmail.com>
2018-11-27 15:28:15 +0100

    Ignore require cycle from node-modules

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // @flow
 
+import { YellowBox } from 'react-native';
 import StorybookUI from './storybook.native';
+
+YellowBox.ignoreWarnings(['Require cycle: node_modules']);
 
 export default StorybookUI;
