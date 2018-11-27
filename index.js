1699b756af238e2373d82b3142880b5ee6abb4f9
Robin Cussol <RobinCsl@users.noreply.github.com>
2018-11-27 16:42:36 +0100

    Update ignoreWarnings
    Co-Authored-By: lukewalczak <lukasz.walczak.pwr@gmail.com>

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 import { YellowBox } from 'react-native';
 import StorybookUI from './storybook.native';
 
-YellowBox.ignoreWarnings(['Require cycle: node_modules']);
+YellowBox.ignoreWarnings(['Require cycle: node_modules/@storybook']);
 
 export default StorybookUI;
