71cb43b82f46feaa42c4a0c120aa9e67297bc969
Robin Cussol <me@robincussol.com>
2018-11-28 14:26:18 +0100

    Add: Tests for Button component

---
+++ src/Button/__tests__/index.test.js
@@ -0,0 +1,226 @@
+// @flow
+
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { render, shallow, fireEvent } from 'react-native-testing-library';
+import snapshotDiff from 'snapshot-diff';
+
+import ButtonWeb from '../index.web';
+import ButtonNative from '../index.native';
+import Icon from '../../Icon';
+
+const originalPlatform = Platform.OS;
+const noop = jest.fn();
+const buttonText = 'Button text';
+const buttonSublabel = 'Button sublabel';
+const leftIcon = <Icon name="calendar" />;
+const rightIcon = <Icon name="chevron-right" />;
+const width = 200;
+const disabled = false;
+const href = 'https://lolilol.com';
+const block = false;
+const type = 'info';
+const testID = 'testID';
+
+afterEach(() => {
+  Platform.OS = originalPlatform;
+});
+
+describe('Button - web', () => {
+  Platform.OS = 'web';
+  const { getByText, getAllByProps } = render(
+    <ButtonWeb
+      onPress={noop}
+      type={type}
+      sublabel={buttonSublabel}
+      leftIcon={leftIcon}
+      rightIcon={rightIcon}
+      width={width}
+      disabled={disabled}
+      href={href}
+      block={block}
+      testID={testID}
+    >
+      {buttonText}
+    </ButtonWeb>
+  );
+
+  it('should have the right text', () => {
+    expect(getByText(buttonText)).toBeDefined();
+  });
+
+  it('should not have any sublabel', () => {
+    expect(() => getByText(buttonSublabel)).toThrow('No instances found');
+  });
+
+  it('should have passed props', () => {
+    expect(
+      getAllByProps({
+        onPress: noop,
+        type,
+        sublabel: buttonSublabel,
+        leftIcon,
+        rightIcon,
+        width,
+        disabled,
+        href,
+        block,
+        testID,
+      })
+    ).toBeDefined();
+  });
+
+  it('should render a button by default', () => {
+    const defaultButton = render(
+      <ButtonWeb onPress={noop}>{buttonText}</ButtonWeb>
+    );
+    // $FlowFixMe react-native-testing-library does not allow HTML elements
+    expect(defaultButton.getByType('button')).toBeDefined();
+    // $FlowFixMe react-native-testing-library does not allow HTML elements
+    fireEvent(defaultButton.getByType('button'), 'click');
+  });
+
+  it('should render an <a> tag if href is provided', () => {
+    const withHref = render(
+      <ButtonWeb onPress={noop} href={href}>
+        {buttonText}
+      </ButtonWeb>
+    );
+    // $FlowFixMe react-native-testing-library does not allow HTML elements
+    expect(withHref.getByType('a')).toBeDefined();
+    // $FlowFixMe react-native-testing-library does not allow HTML elements
+    expect(() => fireEvent(withHref.getByType('a'), 'click')).toThrow(
+      'No handler function found for event: click'
+    );
+  });
+
+  it('should match the snapshot', () => {
+    const { output } = shallow(
+      <ButtonWeb
+        onPress={noop}
+        type={type}
+        sublabel={buttonSublabel}
+        leftIcon={leftIcon}
+        rightIcon={rightIcon}
+        width={width}
+        disabled={disabled}
+        href={href}
+        block={block}
+      >
+        {buttonText}
+      </ButtonWeb>
+    );
+    expect(output).toMatchSnapshot();
+  });
+
+  it('should match snapshot diff', () => {
+    const extend = render(
+      <ButtonWeb
+        onPress={noop}
+        type={type}
+        sublabel={buttonSublabel}
+        leftIcon={leftIcon}
+        rightIcon={rightIcon}
+        width={width}
+        disabled={disabled}
+        href={href}
+        block={block}
+      >
+        {buttonText}
+      </ButtonWeb>
+    );
+    const base = render(<ButtonWeb onPress={noop}>{buttonText}</ButtonWeb>);
+
+    expect(snapshotDiff(base, extend)).toMatchSnapshot();
+  });
+});
+
+describe('Button - native', () => {
+  Platform.OS = 'ios';
+  const { getByText, getByType, getAllByProps } = render(
+    <ButtonNative
+      onPress={noop}
+      type={type}
+      sublabel={buttonSublabel}
+      leftIcon={leftIcon}
+      rightIcon={rightIcon}
+      width={width}
+      disabled={disabled}
+      href={href}
+      block={block}
+      testID={testID}
+    >
+      {buttonText}
+    </ButtonNative>
+  );
+
+  it('should have the right text', () => {
+    expect(getByText(buttonText)).toBeDefined();
+  });
+
+  it('should have the right sublabel', () => {
+    expect(getByText(buttonSublabel)).toBeDefined();
+  });
+
+  it('should execute onPress method', () => {
+    fireEvent(getByType(ButtonNative), 'press');
+  });
+
+  it('should have passed props', () => {
+    expect(
+      getAllByProps({
+        onPress: noop,
+        type,
+        sublabel: buttonSublabel,
+        leftIcon,
+        rightIcon,
+        width,
+        disabled,
+        href,
+        block,
+        testID,
+      })
+    ).toBeDefined();
+  });
+
+  it('should match the snapshot', () => {
+    const { output } = shallow(
+      <ButtonNative
+        onPress={noop}
+        type={type}
+        sublabel={buttonSublabel}
+        leftIcon={leftIcon}
+        rightIcon={rightIcon}
+        width={width}
+        disabled={disabled}
+        href={href}
+        block={block}
+      >
+        {buttonText}
+      </ButtonNative>
+    );
+    expect(output).toMatchSnapshot();
+  });
+
+  it('should match snapshot diff', () => {
+    const extend = render(
+      <ButtonNative
+        onPress={noop}
+        type={type}
+        sublabel={buttonSublabel}
+        leftIcon={leftIcon}
+        rightIcon={rightIcon}
+        width={width}
+        disabled={disabled}
+        href={href}
+        block={block}
+      >
+        {buttonText}
+      </ButtonNative>
+    );
+    const base = render(
+      <ButtonNative onPress={noop}>{buttonText}</ButtonNative>
+    );
+    expect(snapshotDiff(base, extend)).toMatchSnapshot();
+  });
+});
