c5929111eb3946573b3215ae6428c60d77128102
Filip Messa <filip.messa@kiwi.com>
2018-11-28 11:34:11 +0100

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 const withTM = require('next-plugin-transpile-modules')
+const { ANALYZE } = process.env
 
 module.exports = withTM({
   transpileModules: ['react-native', 'react-native-web'],
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+
+		// Bundle Analyzer
+		if(ANALYZE) {
+			const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+			config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: isServer ? 8888 : 8889,
+        openAnalyzer: true
+			}))
+
+		}
+
     // Alias all `react-native` imports to `react-native-web`
     config.resolve.alias = {
       'react-native$': 'react-native-web'
