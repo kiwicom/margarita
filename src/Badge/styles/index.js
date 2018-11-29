2d292ba69dcfaa722524a2d8bcf8d681bfc87a6b
Robin Cussol <RobinCsl@users.noreply.github.com>
2018-11-29 09:05:30 +0100

    Add: Badge component (#66)

---
+++ src/Badge/styles/index.js
@@ -0,0 +1,25 @@
+// @flow
+
+import { defaultTokens } from '@kiwicom/orbit-design-tokens';
+
+export const wrapperColor = {
+  primary: defaultTokens.paletteProductNormal,
+  critical: defaultTokens.backgroundBadgeCritical,
+  dark: defaultTokens.backgroundBadgeDark,
+  info: defaultTokens.backgroundBadgeInfo,
+  neutral: defaultTokens.backgroundBadgeNeutral,
+  success: defaultTokens.backgroundBadgeSuccess,
+  warning: defaultTokens.backgroundBadgeWarning,
+  white: defaultTokens.backgroundBadgeWhite,
+};
+
+export const textColor = {
+  primary: defaultTokens.colorTextWhite,
+  critical: defaultTokens.colorTextBadgeCritical,
+  dark: defaultTokens.colorTextBadgeDark,
+  info: defaultTokens.colorTextBadgeInfo,
+  neutral: defaultTokens.colorTextBadgeNeutral,
+  success: defaultTokens.colorTextBadgeSuccess,
+  warning: defaultTokens.colorTextBadgeWarning,
+  white: defaultTokens.colorTextBadgeWhite,
+};
--- storybook.native/story-loader.js
+++ storybook.native/story-loader.js
@@ -6,23 +6,27 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/Badge/Badge.stories');
   require('../src/Button/Button.stories');
-  require('../src/Icon/Icon.stories');
   require('../src/Checkbox/Checkbox.stories');
+  require('../src/Icon/Icon.stories');
   require('../src/FilterButton/FilterButton.stories');
   require('../src/ServiceLogo/ServiceLogo.stories');
   require('../src/Text/Text.stories');
   require('../src/TextInput/TextInput.stories');
+  
 }
 
 const stories = [
+  '../src/Badge/Badge.stories',
   '../src/Button/Button.stories',
-  '../src/Icon/Icon.stories',
   '../src/Checkbox/Checkbox.stories',
+  '../src/Icon/Icon.stories',
   '../src/FilterButton/FilterButton.stories',
   '../src/ServiceLogo/ServiceLogo.stories',
   '../src/Text/Text.stories',
   '../src/TextInput/TextInput.stories',
+  
 ];
 
 module.exports = {
