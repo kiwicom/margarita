b7de4cfd3826d6c4f0a90ef0807c45d23104ae0a
Trond Bergquist <trond_bergquist@hotmail.com>
2018-11-29 12:54:49 +0100

    Move files that are not routes out of pages
    create new relay package

---
+++ packages/relay/src/QueryRenderer.js
@@ -0,0 +1,61 @@
+// @flow
+
+/* eslint-disable no-restricted-imports */
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {
+  QueryRenderer,
+  type ReadyState,
+  type GraphQLTaggedNode,
+} from 'react-relay';
+
+import environment from './Environment';
+
+type Props = {|
+  +render: (props: Object) => React.Element<any>,
+  +query: GraphQLTaggedNode,
+  +variables?: Object,
+|};
+
+export default class App extends React.Component<Props> {
+  renderRelayContainer = ({ error, props }: ReadyState) => {
+    if (error) {
+      return (
+        <View>
+          <Text style={styles.text}>{error.message}</Text>
+        </View>
+      );
+    }
+    if (!props) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.text}>Loading</Text>
+        </View>
+      );
+    }
+    return this.props.render(props);
+  };
+
+  render() {
+    return (
+      <QueryRenderer
+        variables={this.props.variables}
+        environment={environment}
+        query={this.props.query}
+        render={this.renderRelayContainer}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    alignItems: 'center',
+    fontSize: 24,
+  },
+});
