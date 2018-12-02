7c44ed2fcb52289fa88ed24513f004d7dd91e68f
Filip Messa <filip.messa@kiwi.com>
2018-12-02 14:23:42 +0100

    create workspace for the margarita components

--- app/src/App.js
+++ app/src/App.js
@@ -3,8 +3,8 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { QueryRenderer, graphql } from '@kiwicom/margarita-relay';
+import { Text } from '@kiwicom/margarita-components';
 
-import Text from './components/Text';
 import type { AppQueryResponse } from './__generated__/AppQuery.graphql';
 
 type Props = Object;
