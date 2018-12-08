722a6f6196c24153705a8562c0f62d4a028a9389
Filip Messa <filip.messa@kiwi.com>
2018-12-08 22:13:11 +0100

    remove graphql schema from index.js to separate file

---
+++ packages/graphql/src/Schema.js
@@ -0,0 +1,28 @@
+// @flow
+
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import GlobalID from '@kiwicom/graphql-global-id';
+
+import fetch from './services/Fetch';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: {
+      id: GlobalID(() => 'TODO: implement me'),
+      hello: {
+        type: GraphQLString,
+        description: 'A simple type for getting started!',
+        resolve: async () => {
+          const test = await fetch('/locations/query?term=oslo');
+
+          return `Welcome to Tequila client demo! Test location query = ${
+            test.locations[0].id
+          }`;
+        },
+      },
+    },
+  }),
+});
+
+export default schema;
