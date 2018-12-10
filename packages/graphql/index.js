06455a179940dc4688029d05a131701eb65bf504
Filip Messa <filip.messa@kiwi.com>
2018-12-10 15:35:31 +0100

    move the creation of the server to the server.js file

--- packages/graphql/index.js
+++ packages/graphql/index.js
@@ -1,12 +1,5 @@
 // @flow
 
-import '@babel/polyfill';
-
-import { ApolloServer } from 'apollo-server';
-
-import createContext from './src/services/GraphQLContext';
 import schema from './src/Schema';
 
-const server = new ApolloServer({ schema, context: createContext() });
-
-export { schema, server };
+export { schema };
