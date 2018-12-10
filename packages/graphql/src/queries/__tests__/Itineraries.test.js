0df3424aeb43a0c6d6bd61ff9c2b6ff7ed33a2c1
Trond Bergquist <trond_bergquist@hotmail.com>
2018-12-10 16:20:14 +0100

    Add test for itineraries query

---
+++ packages/graphql/src/queries/__tests__/Itineraries.test.js
@@ -0,0 +1,44 @@
+// @flow
+
+import { graphql } from '../../services/TestingTools';
+
+it('works', async () => {
+  fetch.mockResponseOnce(JSON.stringify({ data: [{ id: 1 }, { id: 2 }] }));
+  const query = `query($input: ItinerariesSearchInput!) {
+
+    searchItineraries(input: $input) {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }`;
+  expect(
+    await graphql(query, {
+      input: {
+        travelFrom: 'OSL',
+        dateFrom: '2018-12-31',
+      },
+    }),
+  ).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "searchItineraries": Object {
+      "edges": Array [
+        Object {
+          "node": Object {
+            "id": "SXRpbmVyYXJ5OjE=",
+          },
+        },
+        Object {
+          "node": Object {
+            "id": "SXRpbmVyYXJ5OjI=",
+          },
+        },
+      ],
+    },
+  },
+}
+`);
+});
