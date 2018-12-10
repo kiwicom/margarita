c46eede23fb106d0927546347ab8dc475d0116bf
Filip Messa <filip.messa@kiwi.com>
2018-12-10 15:35:31 +0100

    move the creation of the server to the server.js file

--- packages/graphql/server.js
+++ packages/graphql/server.js
@@ -1,6 +1,13 @@
 // @flow
 
-import { server } from './index';
+import '@babel/polyfill';
+
+import { ApolloServer } from 'apollo-server';
+
+import createContext from './src/services/GraphQLContext';
+import schema from './src/Schema';
+
+const server = new ApolloServer({ schema, context: createContext() });
 
 server.listen().then(({ url, context }) => {
   console.log(`🚀 Server ready at ${url}`); // eslint-disable-line no-console
