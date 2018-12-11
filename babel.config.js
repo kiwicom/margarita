95f3f32f1efc5e679e9c1efbf65b5a2683a7055a
Robin Cussol <me@robincussol.com>
2018-12-11 11:37:29 +0100

    Add: Documentation about some Expo/workspace quirks

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,9 @@
 |};
 */
 
+// A repo should only contain one babel.config.js, and workspaces should use .babelrc, however there is currently a 🐛
+// https://github.com/martpie/next-plugin-transpile-modules/issues/1#issuecomment-425651334 so .babelrc is not loaded
+
 module.exports = function(api /* :API */) {
   api.cache(true);
   const presets = ['babel-preset-expo', 'module:react-native-dotenv'];
