c1a7a31f19c15e6a27740920365899d996e5bebc
Trond Bergquist <trond_bergquist@hotmail.com>
2018-12-12 19:03:14 +0100

    Use margarita text component, add fonts

--- apps/web/pages/results.js
+++ apps/web/pages/results.js
@@ -1,13 +1,14 @@
 // @flow
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import {
   AnywhereResults,
   EmptyResults,
   ResultsList,
 } from '@kiwicom/margarita-core';
 import { Layout } from '@kiwicom/margarita-components';
+import { Text } from '@kiwicom/universal-components';
 
 export default () => (
   <Layout>
---
+++ packages/tools/index.js
@@ -0,0 +1,4 @@
+// @flow
+
+/* Probably to be loaded from universal-components later */
+export { default as Roboto } from './fonts/Roboto/Roboto-Regular.ttf';
