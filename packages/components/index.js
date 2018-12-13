786b2bab389fcd4a1f14a299d9524e67ad92616f
Trond Bergquist <trond_bergquist@hotmail.com>
2018-12-13 08:28:43 +0100

    Use universal-component buttons
    Add TouchableWithoutFeedback component

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,3 +1,6 @@
 // @flow
 
 export { default as Layout } from './src/Layout';
+export {
+  default as TouchableWithoutFeedback,
+} from './src/TouchableWithoutFeedback';
--- packages/components/src/Navbar.js
+++ packages/components/src/Navbar.js
@@ -1,9 +1,10 @@
 // @flow
 
 import * as React from 'react';
-import { View, Image, TouchableWithoutFeedback } from 'react-native';
+import { View, Image } from 'react-native';
 import { StyleSheet } from '@kiwicom/universal-components';
 import Router from 'next/router';
+import { TouchableWithoutFeedback } from '@kiwicom/margarita-components';
 
 /**
  * This component should only be used by web.
