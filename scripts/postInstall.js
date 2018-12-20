03b2b3a73048408d2e97697373bce57f8005b119
Robin Cussol <me@robincussol.com>
2018-12-20 12:22:22 +0100

    chore: release 0.0.7-alpha.3

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -7,21 +7,37 @@ const log = message => console.log(`➤ ${message}`); // eslint-disable-line no-
 
 // Patching Intl package is needed to work proprely with react-native >= 0.57
 log('Patching intl package');
+
+// In development, intl is located in ../node_modules
+const localPath = path.join(__dirname, '..', 'node_modules', 'intl');
+
+// In packaged version, intl is located in ../../ (as our package is inside node_modules folder)
+const npmPath = path.join(__dirname, '..', '..', 'intl');
+
+let correctPath;
+
+if (fs.existsSync(localPath)) {
+  correctPath = localPath;
+} else if (fs.existsSync(npmPath)) {
+  correctPath = npmPath;
+} else {
+  throw new Error(
+    '@kiwicom/universal-components: impossible to execute postInstall.js\n > path to Intl package not found'
+  );
+}
+
 const intlPackageJson = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, '..', 'node_modules', 'intl', 'package.json'),
-    'utf-8'
-  )
+  fs.readFileSync(path.join(correctPath, 'package.json'), 'utf-8')
 );
 delete intlPackageJson.browser;
 
 fs.writeFileSync(
-  path.join(__dirname, '..', 'node_modules', 'intl', 'package.json'),
+  path.join(correctPath, 'package.json'),
   JSON.stringify(intlPackageJson, null, 2)
 );
 
 try {
-  fs.unlinkSync(path.join(__dirname, '..', 'node_modules', 'intl', '.babelrc'));
+  fs.unlinkSync(path.join(correctPath, '.babelrc'));
 } catch (e) {
   // Ignore error - file doesn't exist
 }
