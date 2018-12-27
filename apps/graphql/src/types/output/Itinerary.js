b73205257baa5e8fe4ab98f0768a983a07d0b696
Jan Václavík <jvaclavik@gmail.com>
2018-12-27 19:56:11 +0100

    Implement ConnectedCard in flight results (#26)

--- apps/graphql/src/types/output/Itinerary.js
+++ apps/graphql/src/types/output/Itinerary.js
@@ -1,21 +1,45 @@
 // @flow
 
-import { GraphQLObjectType, GraphQLInt, GraphQLString } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} from 'graphql';
 import GlobalID from '@kiwicom/graphql-global-id';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 export type Itinerary = {|
   +id: string,
 |};
 
+const routesType = new GraphQLObjectType({
+  name: 'Routes',
+  fields: {
+    airline: { type: GraphQLString },
+    cityFrom: { type: GraphQLString },
+    cityTo: { type: GraphQLString },
+    id: { type: GraphQLString },
+    localArrival: { type: GraphQLDateTime },
+    utcArrival: { type: GraphQLDateTime },
+    localDeparture: {
+      type: GraphQLDateTime,
+    },
+    utcDeparture: { type: GraphQLDateTime },
+  },
+});
+
 export default new GraphQLObjectType({
   name: 'Itinerary',
   fields: {
+    currency: { type: GraphQLString },
     id: GlobalID(({ id }) => id),
     price: { type: GraphQLInt },
     flyFrom: { type: GraphQLString },
     flyTo: { type: GraphQLString },
     localDeparture: { type: GraphQLString },
     localArrival: { type: GraphQLString },
+    routes: { type: new GraphQLList(routesType) },
     // TODO: Add fields as needed
   },
 });
