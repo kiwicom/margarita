bf0189d73f905bb24d98616e57ab7461693f32b7
Robin Cussol <me@robincussol.com>
2019-01-04 16:15:35 +0100

    Feat: Adding SearchParamsSummary component Summary: AdaptableBadge had to be added for this move to be possible; this is just a temporary fix and should eventually live inside @kiwicom/universal-components (it's very similar to current implementation of FilterButton there for instance, so refactor is necessary). I left the stories file for posterity; we might want to add some Storybook-like capacity to the component library specific to margarita.

---
+++ packages/components/src/SearchParamsSummary/SearchParamsSummary.stories.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * KEEPING THIS FILE FOR POSTERITY FOR NOW
+ * NEED TO DECIDE IF WE WOULD LIKE A STORYBOOK-like ENVIRONMENT FOR THIS INTERNAL COMPONENT LIBRARY TOO.
+ *
+ */
+
+// // @flow
+
+// import React from 'react';
+// import { storiesOf } from '@storybook/react-native';
+// import { withKnobs, select } from '@storybook/addon-knobs';
+
+// import SearchParamsSummary from './SearchParamsSummary';
+
+// storiesOf('SearchParamsSummary', module)
+//   .addDecorator(withKnobs)
+//   .add('Playground', () => {
+//     const tripType = select(
+//       'Trip Type',
+//       ['OneWay', 'MultiCity', 'Return'],
+//       'MultiCity'
+//     );
+
+//     return (
+//       <SearchParamsSummary
+//         tripType={tripType}
+//         departure={{ city: 'Wroclaw', localizedDate: 'Oct 10' }}
+//         arrival={{ city: 'Prague', localizedDate: 'Dec 12' }}
+//       />
+//     );
+//   });
