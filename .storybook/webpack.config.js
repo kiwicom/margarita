28639100b9a3d0ab642874e164ed235e6c54a137
Robin Cussol <me@robincussol.com>
2019-01-09 12:05:57 +0100

    Chore: Removed dependency over css-loader
    Summary: css-loader was used notably to load the custom `orbit-icons.ttf` font file. The NPM package provides the font and it needs to be appropriately loaded by the customer. It could be worth providing the font as a different package though.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -18,10 +18,6 @@ module.exports = {
             }
         }]
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
     ],
   },
   resolve: {
--- src/Icon/Icon.css.native.js
+++
@@ -1,3 +0,0 @@
-// @flow
-
-// This is a trick to import css only on the web version, for the icon font to work correctly.
