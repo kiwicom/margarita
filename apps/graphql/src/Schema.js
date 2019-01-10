76be50711b593ff27aab31815fb02bbbf832be03
Filip Messa <filip.messa@kiwi.com>
2019-01-10 10:31:36 +0100

    Chore: Upgrade @kiwicom/graphql-global-id v0.4.0

--- apps/graphql/src/Schema.js
+++ apps/graphql/src/Schema.js
@@ -10,21 +10,8 @@ const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
-      id: GlobalID(() => 'TODO: implement me'),
       searchItineraries: Itineraries,
       locationsByTerm: Locations,
-
-      hello: {
-        type: GraphQLString,
-        description: 'A simple type for getting started!',
-        resolve: async (_: mixed, __: mixed, { dataLoader }: Object) => {
-          const test = await dataLoader.locations.load({ term: 'OSL' });
-
-          return `Welcome to Tequila client demo! Test location query = ${
-            test.locations[0].id
-          }`;
-        },
-      },
     },
   }),
 });
