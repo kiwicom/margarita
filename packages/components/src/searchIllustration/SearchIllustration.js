006da330a7013b62d4a142f9342fe05d5b897f65
Robin Cussol <me@robincussol.com>
2019-01-17 12:30:41 +0100

    Fix: Search Illustration is fetched from CDN
    Summary: The way the component was built relied on logic that could break easily: if anyone (re)moved the files in apps/web/static, the component would break without any warning.
    Orbit components' Illustration component fetches from Kiwi.com's image CDN, and I believe this is more robust and extensible.

--- packages/components/src/searchIllustration/SearchIllustration.js
+++ packages/components/src/searchIllustration/SearchIllustration.js
@@ -1,14 +1,28 @@
 // @flow
 
 import React from 'react';
+import { defaultTokens } from '@kiwicom/orbit-design-tokens';
 
-const baseImagePath = './static/img/search-travel';
+/**
+ * This component could be extracted to include more illustrations.
+ * However, that would require fetching and bundling all the assets for mobile.
+ */
+
+const baseImagePath = (resolution: number) =>
+  `//images.kiwi.com/illustrations/0x${parseInt(
+    defaultTokens.heightIllustrationMedium,
+    10,
+  ) * resolution}/Boarding.png`;
+
+const RESOLUTIONS = [1, 2, 3];
 
 export default function SearchIllustration() {
   return (
     <img
-      src={`${baseImagePath}.png`}
-      srcSet={`${baseImagePath}.png 1x, ${baseImagePath}@2x.png 2x, ${baseImagePath}@3x.png 3x`}
+      src={baseImagePath(1)}
+      srcSet={RESOLUTIONS.map(
+        resolution => `${baseImagePath(resolution)} ${resolution}x`,
+      ).join(',')}
       style={{ width: '80%', alignSelf: 'center', marginBottom: '20px' }}
     />
   );
