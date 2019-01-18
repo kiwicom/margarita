b4b91bbbd7862991ba7c2f3ea7a76259e6c946b6
Filip Messa <filip.messa@kiwi.com>
2019-01-18 13:38:20 +0100

    TEMP: added default limit for the Tequila's search API

--- apps/graphql/src/dataloaders/__tests__/Itinerariesloader.test.js
+++ apps/graphql/src/dataloaders/__tests__/Itinerariesloader.test.js
@@ -1,32 +1,33 @@
 // @flow
 
-import { parseParameters } from '../Itinerariesloader';
+import { parseParameters } from "../Itinerariesloader";
 
 const searchParams = {
-  travelFrom: 'OSL',
-  travelTo: 'PRG',
-  dateFrom: new Date('2018-01-01'),
+  travelFrom: "OSL",
+  travelTo: "PRG",
+  dateFrom: new Date("2018-01-01")
 };
 
-it('parses parameters corectly', () => {
+it("parses parameters corectly", () => {
   expect(parseParameters(searchParams)).toMatchInlineSnapshot(`
 Object {
   "curr": "EUR",
   "dateFrom": "01/01/2018",
   "dateTo": "01/01/2018",
   "flyFrom": "OSL",
+  "limit": 10,
   "to": "PRG",
 }
 `);
   expect(
     parseParameters({
       ...searchParams,
-      returnDateFrom: new Date('2018-01-02'),
-      returnDateTo: new Date('2018-01-02'),
+      returnDateFrom: new Date("2018-01-02"),
+      returnDateTo: new Date("2018-01-02"),
       passengers: {
-        adults: 2,
-      },
-    }),
+        adults: 2
+      }
+    })
   ).toMatchInlineSnapshot(`
 Object {
   "adults": 2,
@@ -36,6 +37,7 @@ Object {
   "dateTo": "01/01/2018",
   "flyFrom": "OSL",
   "infants": 0,
+  "limit": 10,
   "returnFrom": "02/01/2018",
   "returnTo": "02/01/2018",
   "to": "PRG",
