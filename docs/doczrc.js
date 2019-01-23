20f29326d83e77229cb1897cb63dffa6cf49ec72
Robin Cussol <me@robincussol.com>
2019-01-23 13:57:44 +0100

    Fix: orbit-icons font added to built documentation
    Summary: It is somehow tricky to add custom fonts in Docz, and a few config properties are not fully documented. I managed to make the fonts work only in the built result, not in the dev environment. So this could be improved in the future.

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -4,4 +4,15 @@ export default {
   src: '..',
   dest: '../.docz/dist',
   native: true,
+  public: './public',
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: '/public/font.css',
+        },
+      ],
+    },
+  },
 };
