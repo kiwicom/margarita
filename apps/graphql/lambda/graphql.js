1914059b89654789f6c2bab5200bee2a25346d9c
Robin Cussol <me@robincussol.com>
2019-01-29 14:00:01 +0100

    Feat: Added serverless deployment config
    Summary: Currently, the deployment process is manual. Adding it to CircleCI will be done in a separate PR.
    I removed the function build and deploy on Netlify.
    
    Assuming you have AWS credentials with aws-cli configured and that you are in apps/graphql, you would just need to do:
    - `yarn webpack`: this builds the server into a Node-compatible JavaScript
    - `serverless deploy function --function margarita-graphql`: this will update the Lambda function
    
    We could probably add a test to make sure the deployment was successful, by sending a POST request to the endpoint for example. If it did not work, it should somehow rollback (though I am not sure how that works precisely)

--- apps/graphql/lambda/graphql.js
+++ apps/graphql/lambda/graphql.js
@@ -1,22 +1,45 @@
 // @flow
 
 import { ApolloServer } from 'apollo-server-lambda';
+import type { APIGatewayEvent, Context, ProxyCallback } from 'flow-aws-lambda';
 
 import createContext from '../src/services/GraphQLContext';
 import schema from '../src/Schema';
 
-const server = new ApolloServer({
-  schema,
-  context: () => {
-    // Please note: this context must be created for every single request.
-    // This is important because of these reasons:
-    //   - tokens and user identities are per request
-    //   - dataloaders use Map internally (not LRU) and they would otherwise
-    //     grow indefinitely because the Map content is not garbage collected
-    return createContext();
-  },
-  playground: true,
-  introspection: true,
-});
+const server = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: ProxyCallback,
+) =>
+  new ApolloServer({
+    schema,
+    context: {
+      headers: event.headers,
+      functionName: context.functionName,
+      event,
+      context,
+      // Please note: this context must be created for every single request.
+      // This is important because of these reasons:
+      //   - tokens and user identities are per request
+      //   - dataloaders use Map internally (not LRU) and they would otherwise
+      //     grow indefinitely because the Map content is not garbage collected
+      ...createContext(),
+    },
+    playground: {
+      endpoint: '/dev/graphql',
+    },
+    introspection: true,
+  });
 
-exports.handler = server.createHandler();
+exports.handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: ProxyCallback,
+) => {
+  return server(event, context, callback).createHandler({
+    cors: {
+      origin: '*',
+      allowedHeaders: ['Content-Type', 'Origin', 'Accept'],
+    },
+  })(event, context, callback);
+};
---
+++ apps/graphql/webpack.config.js
@@ -0,0 +1,45 @@
+// @flow
+
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  target: 'node',
+  plugins: [new webpack.IgnorePlugin(/\/iconv-loader$/)],
+  optimization: {
+    nodeEnv: false,
+    namedModules: true,
+    namedChunks: true,
+    minimizer: [new UglifyJsPlugin()],
+  },
+  entry: {
+    index: './lambda/graphql.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'lambda', 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      { test: /\.json$/, exclude: /node_modules/, loader: 'json-loader' },
+      {
+        // see: https://github.com/apollographql/react-apollo/issues/1737
+        test: /\.mjs$/,
+        type: 'javascript/auto',
+        use: [],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true,
+          },
+        },
+      },
+    ],
+  },
+};
