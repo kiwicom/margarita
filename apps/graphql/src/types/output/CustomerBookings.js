91f11c7a9f7135b0eeb67aa70ccea4e4b12f2f00
Trond Bergquist <trond_bergquist@hotmail.com>
2019-02-01 13:18:41 +0100

    Add arrival, departure, destinationImageUrl and passenger count
    These are the fields necessary for creating the MMB bookingslist

--- apps/graphql/src/types/output/CustomerBookings.js
+++ apps/graphql/src/types/output/CustomerBookings.js
@@ -1,7 +1,15 @@
 // @flow
 
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLEnumType,
+  GraphQLInt,
+} from 'graphql';
 import globalID from '@kiwicom/graphql-global-id';
+
+import GraphQLRouteStop from './RouteStop';
+import type { Booking } from '../../dataloaders/BookingsLoader';
 
 export default new GraphQLObjectType({
   name: 'CustomerBooking',
@@ -10,6 +18,41 @@ export default new GraphQLObjectType({
     id: globalID(({ bid }) => bid),
     status: {
       type: GraphQLString,
+    },
+    arrival: {
+      type: GraphQLRouteStop,
+    },
+    departure: {
+      type: GraphQLRouteStop,
+    },
+    destinationImageUrl: {
+      type: GraphQLString,
+      args: {
+        dimensions: {
+          type: new GraphQLEnumType({
+            name: 'BookingDestinationImageDimensions',
+            values: {
+              _1200x628: { value: '1200x628' },
+              _1280x720: { value: '1280x720' },
+              _220x165: { value: '220x165' },
+              _275x250: { value: '275x250' },
+              _300x165: { value: '300x165' },
+              _375x165: { value: '375x165' },
+              _600x330: { value: '600x330' },
+              _600x600: { value: '600x600' },
+              _610x251: { value: '610x251' },
+            },
+          }),
+          defaultValue: '600x600',
+        },
+      },
+      resolve: (
+        { arrival: { cityId } }: Booking,
+        args: {| +dimensions: string |},
+      ) => `https://images.kiwi.com/photos/${args.dimensions}/${cityId}.jpg`,
+    },
+    passengerCount: {
+      type: GraphQLInt,
     },
   },
 });
