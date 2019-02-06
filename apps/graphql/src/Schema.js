aeb783d307c0a03e8d24a926d758c7c65838180f
Trond Bergquist <trond_bergquist@hotmail.com>
2019-02-06 09:02:17 +0100

    Graphql: Add bookingDetail query
    Fetch details about one booking

--- apps/graphql/src/Schema.js
+++ apps/graphql/src/Schema.js
@@ -5,11 +5,13 @@ import { GraphQLSchema, GraphQLObjectType } from 'graphql';
 import Itineraries from './queries/Itineraries';
 import Locations from './queries/Locations';
 import CustomerBookings from './queries/CustomerBookings';
+import BookingDetail from './queries/BookingDetail';
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
+      bookingDetail: BookingDetail,
       customerBookings: CustomerBookings,
       locationsByTerm: Locations,
       searchItineraries: Itineraries,
