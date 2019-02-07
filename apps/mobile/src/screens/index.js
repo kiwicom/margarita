cd841210ca7c88ea50754b4d37cccbf4b57717dc
Filip Messa <filip.messa@kiwi.com>
2019-02-07 17:30:42 +0100

    Remove: remove the place picker page

--- apps/mobile/src/screens/index.js
+++ apps/mobile/src/screens/index.js
@@ -2,7 +2,6 @@
 
 export { default as SearchScreen } from './SearchScreen';
 export { default as ResultsScreen } from './ResultsScreen';
-export { default as PlacePickerScreen } from './PlacePickerScreen';
 export { default as BookingsListScreen } from './BookingsListScreen';
 export { default as BookingDetailScreen } from './BookingDetailScreen';
 export { default as ShareBookingScreen } from './ShareBookingScreen';
