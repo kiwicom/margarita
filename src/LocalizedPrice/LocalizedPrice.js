0420d1773402aa92b8775e6bedbce462e4f30d30
JoseDuSV <josefduda@gmail.com>
2019-02-12 18:20:25 +0100

    Feat: Updated LocalizedPrice web styles
    Summary: Added component web style. Style can now also be further extended with `style` prop.

--- src/LocalizedPrice/LocalizedPrice.js
+++ src/LocalizedPrice/LocalizedPrice.js
@@ -5,17 +5,27 @@ import { defaultTokens } from '@kiwicom/orbit-design-tokens';
 
 import { Text } from '../Text';
 import { StyleSheet } from '../PlatformStyleSheet';
-import type { LocalizedPriceProps } from './LocalizedPriceTypes';
+import type { StylePropType } from '../PlatformStyleSheet/StyleTypes';
+
+type LocalizedPriceProps = {|
+  +localizedPrice: string,
+  +style?: StylePropType,
+|};
 
 export default function LocalizedPrice({
   localizedPrice,
+  style,
 }: LocalizedPriceProps) {
-  return <Text style={styles.price}>{localizedPrice}</Text>;
+  return <Text style={[styles.price, style]}>{localizedPrice}</Text>;
 }
 const styles = StyleSheet.create({
   price: {
     fontWeight: 'bold',
     color: defaultTokens.colorTextLinkPrimary,
     fontSize: parseFloat(defaultTokens.fontSizeTextLarge),
+    web: {
+      color: '#2e353b', // // @TODO repeating value - should be added to design-tokens
+      fontSize: 18,
+    },
   },
 });
--- src/LocalizedPrice/LocalizedPriceTypes.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-export type LocalizedPriceProps = {|
-  +localizedPrice: string,
-|};
