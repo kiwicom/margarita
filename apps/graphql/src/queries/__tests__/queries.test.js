fd8f31f927a3aaf2ed40bccef5641951af8659b8
Trond Bergquist <trond_bergquist@hotmail.com>
2019-02-18 17:02:59 +0100

    Graphql: Replace Itineraries.test with fixture test

--- apps/graphql/src/queries/__tests__/queries.test.js
+++ apps/graphql/src/queries/__tests__/queries.test.js
@@ -3,6 +3,9 @@
 import { generateTestsFromFixtures } from '@kiwicom/test-utils';
 
 import executeTestQuery from '../../executeTestQuery';
+import Itineraries from '../../datasets/Itineraries.json';
+
+fetch.mockResponses([JSON.stringify(Itineraries)]);
 
 generateTestsFromFixtures(`${__dirname}/__fixtures__`, input =>
   executeTestQuery(input),
