125a703b17da828f3e932a8312ca776f52d74752
Trond Bergquist <trond_bergquist@hotmail.com>
2019-02-20 15:10:23 +0100

    MMB: Add expand button with icon under sector info
    Add animation for icon on click

--- apps/core/scenes/bookingDetail/sectorDetails/SectorDetails.js
+++ apps/core/scenes/bookingDetail/sectorDetails/SectorDetails.js
@@ -4,11 +4,13 @@ import * as React from 'react';
 import { Card, StyleSheet } from '@kiwicom/universal-components';
 import { Separator, BookingTypeRenderer } from '@kiwicom/margarita-components';
 import { createFragmentContainer, graphql } from '@kiwicom/margarita-relay';
+import { defaultTokens } from '@kiwicom/orbit-design-tokens';
 
 import Header from './Header';
 import SectorInfoOneWay from './SectorInfoOneWay';
 import SectorInfoMulticity from './SectorInfoMulticity';
 import SectorInfoReturn from './SectorInfoReturn';
+import SegmentContainer from './segments/SegmentContainer';
 import type { SectorDetails as BookingType } from './__generated__/SectorDetails.graphql';
 
 type Props = {|
@@ -27,6 +29,8 @@ function SectorDetails(props: Props) {
         returnComponent={<SectorInfoReturn data={props.data} />}
         multicityComponent={<SectorInfoMulticity data={props.data} />}
       />
+      <Separator style={styles.bottomSeparator} />
+      <SegmentContainer />
     </Card>
   );
 }
@@ -34,10 +38,17 @@ function SectorDetails(props: Props) {
 const styles = StyleSheet.create({
   card: {
     paddingHorizontal: 18,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: defaultTokens.paletteInkLighter,
+    paddingBottom: 0,
   },
   separator: {
     marginTop: 7,
     marginBottom: 17.5,
+  },
+  bottomSeparator: {
+    marginHorizontal: -18,
+    marginTop: 16,
   },
 });
 
---
+++ apps/core/scenes/bookingDetail/sectorDetails/segments/SegmentContainer.js
@@ -0,0 +1,86 @@
+// @flow
+
+import * as React from 'react';
+import {
+  Touchable,
+  Icon,
+  Text,
+  StyleSheet,
+} from '@kiwicom/universal-components';
+import { defaultTokens } from '@kiwicom/orbit-design-tokens';
+import { Animated } from 'react-native';
+
+type Props = {||};
+
+type State = {|
+  +expandSegments: boolean,
+  +isDisabled: boolean,
+|};
+
+export default class SegmentContainer extends React.Component<Props, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      expandSegments: false,
+      isDisabled: false,
+    };
+
+    this.spinValue = new Animated.Value(0);
+  }
+
+  spinValue: Animated.Value;
+
+  toggleExpanded = () => {
+    this.setState({ isDisabled: true }, () => {
+      Animated.timing(this.spinValue, {
+        toValue: this.state.expandSegments ? 0 : 1,
+        duration: 250,
+        useNativeDriver: true,
+      }).start(() => {
+        this.setState(state => ({
+          expandSegments: !state.expandSegments,
+          isDisabled: false,
+        }));
+      });
+    });
+  };
+
+  render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg'],
+    });
+
+    return (
+      <>
+        <Touchable
+          onPress={this.toggleExpanded}
+          disabled={this.state.isDisabled}
+        >
+          <Animated.View
+            style={[
+              styles.iconWrapper,
+              {
+                transform: [{ rotate: spin }, { perspective: 1000 }],
+              },
+            ]}
+          >
+            <Icon
+              name="chevron-down"
+              color={defaultTokens.paletteProductNormal}
+            />
+          </Animated.View>
+        </Touchable>
+        {this.state.expandSegments && <Text>TODO: Put segment data</Text>}
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  iconWrapper: {
+    flex: 1,
+    alignSelf: 'center',
+  },
+});
