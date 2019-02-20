4bd8e11633e8f25c53696a1903982c7dc949ea01
Trond Bergquist <trond_bergquist@hotmail.com>
2019-02-20 11:38:11 +0100

    Components: Rename folders to camelCase
    closes #315

--- packages/components/src/placeCard/__tests__/index.test.js
+++ packages/components/src/placeCard/__tests__/index.test.js
@@ -5,7 +5,7 @@ import { render, fireEvent } from 'react-native-testing-library';
 import { Touchable } from '@kiwicom/universal-components';
 
 import PlaceCard from '../PlaceCard';
-import MissingImage from '../Image/MissingImage';
+import MissingImage from '../image/MissingImage';
 
 describe('PlaceCard', () => {
   const onPress = jest.fn();
