dcfbdaed264833695f704aa354dd8250ee0163f2
Filip Messa <messa.filip@gmail.com>
2019-02-27 14:27:46 +0100

    Chore: Bump next-plugin-transpile-modules from 0.1.3 to 2.0.0

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -34,8 +34,10 @@ module.exports = withImages(
 
       // Alias all `react-native` imports to `react-native-web`
       config.resolve.alias = {
+        ...(config.resolve.alias || {}),
         'react-native$': 'react-native-web',
       };
+
       config.resolve.extensions = [
         ...config.resolve.extensions,
         '.web.js',
