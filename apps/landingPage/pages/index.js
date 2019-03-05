be068e8942d58d971f3e02cc23d7eea08b3fc080
KarinaDavtyan <k.davtyan27@gmail.com>
2019-03-05 15:13:06 +0100

    Update header (#481)

--- apps/landingPage/pages/index.js
+++ apps/landingPage/pages/index.js
@@ -12,6 +12,7 @@ export default function App() {
       <Head>
         <title>Margarita</title>
         <link rel="icon" type="image/png" href="/static/logo_margarita.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <GlobalStyle />
       <LandingPage />
