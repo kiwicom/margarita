1ceb83c6d86dc0eeb8c7605ddc76341a0bde560e
Robin Cussol <me@robincussol.com>
2019-03-06 10:30:38 +0100

    Chore: Deploying landing page to S3

--- apps/landingPage/next.config.js
+++ apps/landingPage/next.config.js
@@ -2,9 +2,6 @@
 
 const withTM = require('next-transpile-modules');
 
-const debug = process.env.NODE_ENV !== 'production';
-
 module.exports = withTM({
   transpileModules: ['@kiwicom/margarita-config'],
-  assetPrefix: !debug ? '/margarita/' : '',
 });
