4b65e42aa95f1e96c04ad4bbdae1c4d9e8c94bf4
Jan Václavík <jvaclavik@gmail.com>
2019-03-19 15:38:29 +0100

    Change export format from margarita-config

--- apps/core/components/ItineraryCard/TripSectorHelpers.js
+++ apps/core/components/ItineraryCard/TripSectorHelpers.js
@@ -3,7 +3,6 @@
 import * as DateFNS from 'date-fns';
 import { HOURS_MINUTES_FORMAT } from '@kiwicom/margarita-config';
 
-export const getFormattedDate = (
-  time: ?string,
-  format: ?string = HOURS_MINUTES_FORMAT,
-) => time && format && DateFNS.format(DateFNS.parseISO(time), format);
+export const getFormattedDate = (time: ?string, format: ?string) =>
+  time &&
+  DateFNS.format(DateFNS.parseISO(time), format ?? HOURS_MINUTES_FORMAT);
