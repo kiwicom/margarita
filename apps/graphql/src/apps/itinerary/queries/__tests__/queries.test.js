ec1f0c5a3acf0206bcaa7566cacab20abb2f633f
Albert Alises <albert.alises@gmail.com>
2019-03-21 15:40:59 +0100

    Feat: Create Search One Way GraphQL query (#566)

--- apps/graphql/src/apps/itinerary/queries/__tests__/queries.test.js
+++ apps/graphql/src/apps/itinerary/queries/__tests__/queries.test.js
@@ -5,8 +5,7 @@ import { generateTestsFromFixtures } from '@kiwicom/test-utils';
 import executeTestQuery from '../../../../services/testingTools/executeTestQuery';
 import Itineraries from '../../__datasets__/Itineraries.json';
 
-fetch.mockResponses([JSON.stringify(Itineraries)]);
-
-generateTestsFromFixtures(`${__dirname}/__fixtures__`, input =>
-  executeTestQuery(input),
-);
+generateTestsFromFixtures(`${__dirname}/__fixtures__`, input => {
+  fetch.mockResponses([JSON.stringify(Itineraries)]);
+  return executeTestQuery(input);
+});
