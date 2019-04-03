8b42f48334cfe19821c0f31d4980b54de1fdd7ff
Robin Cussol <me@robincussol.com>
2019-04-03 15:41:55 +0200

    LandingPage: Fix failing Webpack build
    See similar issue https://github.com/kiwicom/margarita/pull/624

--- apps/landingPage/next.config.js
+++ apps/landingPage/next.config.js
@@ -4,4 +4,20 @@ const withTM = require('next-transpile-modules');
 
 module.exports = withTM({
   transpileModules: ['@kiwicom/margarita-config'],
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.js$/,
+      use: [
+        {
+          ...options.defaultLoaders.babel,
+          options: {
+            ...options.defaultLoaders.babel.options,
+            rootMode: 'upward',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 });
