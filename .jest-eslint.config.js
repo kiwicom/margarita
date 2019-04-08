22d212508a4c0d0a060a605c1cb99c86fb63fb20
Trond Bergquist <trond_bergquist@hotmail.com>
2019-04-08 09:07:46 +0200

    Use eslint-runner from @kiwicom/eslint-config

---
+++ .jest-eslint.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  displayName: 'lint',
+  runner: '@kiwicom/eslint-config/runner',
+  testMatch: ['<rootDir>/apps/**/*.js', '<rootDir>/scripts/**/*.js', '<rootDir>/packages/**/*.js'],
+};
