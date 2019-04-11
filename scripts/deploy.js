d191c143beecf221e095ca765c0373827d25e069
Robin Cussol <me@robincussol.com>
2019-04-11 11:23:39 +0200

    Feat: adding back netlify.toml and deploy script
    Summary: This is to better version control the build commands for Netlify, instead of relying on their UI, to which I only have access. This should be merged after https://github.com/kiwicom/margarita/pull/656.
    It adds a deploy script that, based on an environment variable SITE_ID (that has to be set on Netlify's UI, sadly), will deploy the right site. This is a current limitation of `netlify.toml` as it does not support deploying multiple sites from one repository out of the box. (see https://github.com/netlify/netlify-cms/issues/1325 for more information)

---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+// @flow
+
+import { execSync } from 'child_process';
+
+execSync('rimraf deploy', { stdio: 'inherit' });
+
+const { SITE_ID } = process.env;
+
+/**
+ * This script allows to build the right site for Netlify, depending on an environment variable, SITE_ID, set per Netlify website.
+ * All built files should go into <ROOT_FOLDER>/deploy (cf. `publish` field in netlify.toml)
+ */
+
+switch (SITE_ID) {
+  case '@kiwicom/universal-components': {
+    execSync('yarn workspace @kiwicom/universal-components storybook:build', {
+      stdio: 'inherit',
+    });
+    break;
+  }
+  case '@kiwicom/margarita-web': {
+    execSync('yarn create-dotenv', { stdio: 'inherit' });
+    execSync('yarn workspace @kiwicom/margarita-web deploy', {
+      stdio: 'inherit',
+    });
+    break;
+  }
+  default: {
+    console.log('No SITE_ID environment variable was set; exiting.');
+  }
+}
