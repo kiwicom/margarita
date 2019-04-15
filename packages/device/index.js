f099fd63d996a2b541aa1ef5f87bd25e457993ff
Trond Bergquist <trond_bergquist@hotmail.com>
2019-04-15 14:13:57 +0200

    Move GetDeviceLocale to device package

--- packages/device/index.js
+++ packages/device/index.js
@@ -5,3 +5,4 @@ export { LAYOUT } from './src/Layout/LayoutConstants';
 export type { LayoutContextState } from './src/Layout/LayoutContext';
 export { withLayoutContext } from './src/Layout/LayoutContext';
 export { default as LayoutContextProvider } from './src/Layout/LayoutContext';
+export { getLocaleDashed } from './src/GetDeviceLocale/GetDeviceLocale';
