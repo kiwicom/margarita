33eedb14478cceea11e0e25c9f46b1aaf6156c2c
Albert Alises <albert.alises@gmail.com>
2019-05-09 14:49:06 +0200

    Test: Minimum change to graphQL server to test serverless deployment (#753)

--- apps/graphql/src/apps/itinerary/types/inputs/common/ItineraryCommonSearchInputFields.js
+++ apps/graphql/src/apps/itinerary/types/inputs/common/ItineraryCommonSearchInputFields.js
@@ -16,7 +16,7 @@ export default {
     type: SortSearchInput,
   },
   limit: {
-    description: 'Number of results to get from the search (maximum 200)',
+    description: 'Limit of results to get from the search (maximum 200)',
     type: GraphQLInt,
   },
   passengers: {
