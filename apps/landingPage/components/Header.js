5f6618aa573fe805c38da63429c8b7061bbc5972
KarinaDavtyan <k.davtyan27@gmail.com>
2019-05-10 16:45:23 +0200

    Update header of the landing page
    Summary: plane image set as a background for both mobile and desktop. Content height "inside" the image is calculated and set in order to place Logos correctly below it.

--- apps/landingPage/components/Header.js
+++ apps/landingPage/components/Header.js
@@ -1,93 +1,224 @@
 // @flow
 
 import * as React from 'react';
-import { Heading } from '@kiwicom/orbit-components/lib/';
+import {
+  Heading,
+  Text,
+  Button,
+  ButtonLink,
+  ButtonGroup,
+} from '@kiwicom/orbit-components/lib/';
 import styled from 'styled-components';
-import { GITHUB_LINK, CODE_KIWI_LINK } from '@kiwicom/margarita-config';
 import { defaultTokens } from '@kiwicom/orbit-design-tokens';
 
-import { BREAKPOINTS } from '../mediaQueriesConfig';
+import LogoCard from './LogoCard';
 
-const linksHeader = [
-  { title: 'About Margarita', link: '/' },
-  { title: 'Playground', link: '#playground' },
+const logos = [
   {
-    title: 'GitHub',
-    link: GITHUB_LINK,
+    icon: (
+      <img
+        src="/static/react-logo.png"
+        alt="React Native"
+        style={{ height: '48px' }}
+      />
+    ),
+    title: 'React Native',
+    text: 'for IOS, Android and web',
+  },
+  {
+    icon: (
+      <img
+        src="/static/graphql-logo.png"
+        alt="Graphql"
+        style={{ height: '48px' }}
+      />
+    ),
+    title: 'GraphQL',
+    text: 'server',
+  },
+  {
+    icon: (
+      <img
+        src="static/tequila-logo.png"
+        alt="Tequila"
+        style={{ height: '48px' }}
+      />
+    ),
+    title: 'Tequila',
+    text: 'API from Kiwi.com',
   },
 ];
-
-type Link = {|
-  +title: string,
-  +link: string,
-|};
-
-function renderLinks(links: Array<Link>): Array<React.Node> {
-  return links.map<React.Node>(el => (
-    <LinkWithoutStyle key={el.title} href={el.link}>
-      <Heading type="title4">{el.title}</Heading>
-    </LinkWithoutStyle>
-  ));
-}
-
 export default function Header() {
   return (
     <Container>
-      <LinkWithHeight href={CODE_KIWI_LINK}>
-        <Logo src="/static/logo.png" alt="Logo" />
-      </LinkWithHeight>
-      <HeaderRight>{renderLinks(linksHeader)}</HeaderRight>
-      <GithubLink>
-        <LinkWithoutStyle href={GITHUB_LINK}>
-          <Heading type="title3">GitHub</Heading>
-        </LinkWithoutStyle>
-      </GithubLink>
+      <PlaneImageMobile />
+      <UpperHeader>
+        <Logo src="/static/logo_white.png" alt="kiwi.com logo" />
+      </UpperHeader>
+      <Columns>
+        <Headline>
+          <Heading inverted={true} element="div">
+            <WrappedText>
+              Use <ProductName>Margarita</ProductName> and create your own
+              Kiwi.com for free
+            </WrappedText>
+          </Heading>
+          <Text size="large" element="div" weight="bold" spaceAfter="medium">
+            <HeadlineTextWrapper>
+              We prepared everything you will need — data, web and mobile apps.
+              All is based on modern technologies and it's open source.
+            </HeadlineTextWrapper>
+          </Text>
+          <ButtonWrapper>
+            <ButtonGroup>
+              <Button
+                circled
+                width={140}
+                href="https://kiwicom.github.io/margarita/docs/"
+              >
+                Read more
+              </Button>
+              <ButtonLink
+                type="secondary"
+                href="https://github.com/kiwicom/margarita"
+              >
+                <GithubLogo src="/static/github.png" alt="Github" />
+                <Text type="white" weight="bold">
+                  View on GitHub
+                </Text>
+              </ButtonLink>
+            </ButtonGroup>
+          </ButtonWrapper>
+        </Headline>
+        <PhoneImage src="/static/search.png" alt="Booking" />
+      </Columns>
+      <Logos>
+        {logos.map(logo => (
+          <LogoCard
+            key={logo.title}
+            icon={logo.icon}
+            title={logo.title}
+            text={logo.text}
+          />
+        ))}
+      </Logos>
     </Container>
   );
 }
 
+const marginHorizontal = 120;
+const headerMarginTop = parseInt(defaultTokens.spaceSmall, 10);
+const upperHeaderMobile = 84;
+const upperHeaderDesktop = 144;
+const planeImageHeightMobile = 400;
+const planeImageHeightDesktop = 600;
+const contentHeight = (upperHeader, headerMarginTop, planeImageHeight) =>
+  planeImageHeight - headerMarginTop - upperHeader;
+
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-shadow: 0 0 7px rgba(0, 0, 0, 0.4);
-  border-bottom: solid 2px rgb(188, 201, 214);
-  padding: ${defaultTokens.spaceSmall} 0;
-  @media (min-width: ${BREAKPOINTS.BIG_MOBILE}px) {
-    box-shadow: none;
-    border: none;
+  flex-direction: column;
+  margin: ${defaultTokens.spaceSmall} ${defaultTokens.spaceSmall} 0
+    ${defaultTokens.spaceSmall};
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    margin: ${defaultTokens.spaceSmall} ${marginHorizontal}px 0
+      ${marginHorizontal}px;
   }
 `;
 
-const GithubLink = styled.div`
-  padding-right: 10px;
-  cursor: pointer;
-  @media (min-width: ${BREAKPOINTS.BIG_MOBILE}px) {
-    display: none;
+const PlaneImageMobile = styled.div`
+  display: block;
+  position: absolute;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: ${planeImageHeightMobile}px;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url('/static/airport.jpg') no-repeat 40% 90%;
+  background-size: cover;
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    height: ${planeImageHeightDesktop}px;
   }
 `;
 
-const LinkWithHeight = styled.a`
-  height: 42px;
+const UpperHeader = styled.div`
+  z-index: 1;
+  padding-bottom: ${defaultTokens.spaceXXLarge};
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    padding-bottom: 100px;
+  }
+`;
+
+const Columns = styled.div`
+  z-index: 1;
+  flex-direction: row;
+  display: flex;
+  height: ${contentHeight(upperHeaderMobile, headerMarginTop, 400)}px;
+  margin-bottom: ${defaultTokens.spaceXXLarge};
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    height: ${contentHeight(upperHeaderDesktop, headerMarginTop, 600)}px;
+  }
 `;
 
 const Logo = styled.img`
-  height: 42px;
-  padding-left: 2vw;
+  display: block;
+  position: abslolute;
+  height: 36px;
+  padding-top: ${defaultTokens.spaceSmall};
 `;
 
-const HeaderRight = styled.div`
-  display: flex;
-  padding-right: 3vw;
-  align-items: center;
-  justify-content: space-evenly;
-  display: none;
-  @media (min-width: ${BREAKPOINTS.BIG_MOBILE}px) {
-    display: flex;
-    width: 300px;
+const Headline = styled.div`
+  margin: 0;
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    margin-right: 70px;
   }
 `;
 
-const LinkWithoutStyle = styled.a`
-  text-decoration: none;
+const WrappedText = styled.div`
+  white-space: pre-wrap;
+  font-size: 1.3em;
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    font-size: 2em;
+    color: white;
+  }
+`;
+
+const ProductName = styled.span`
+  font-weight: 700;
+  background-color: rgba(255, 255, 255, 0.9);
+  padding: ${defaultTokens.spaceXXSmall}px ${defaultTokens.spaceXSmall}px;
+  color: ${defaultTokens.paletteProductNormal};
+`;
+
+const Logos = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 2em;
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    flex-direction: row;
+  }
+`;
+
+const GithubLogo = styled.img`
+  height: 24px;
+  padding-right: ${defaultTokens.spaceXSmall};
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+`;
+
+const PhoneImage = styled.img`
+  margin-top: -100px;
+  height: 700px;
+  display: none;
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    display: block;
+  }
+`;
+
+const HeadlineTextWrapper = styled.p`
+  text-shadow: 0 0 30px rgba(0, 0, 0, 1), 0 0 30px rgba(0, 0, 0, 1);
+  color: white;
 `;
