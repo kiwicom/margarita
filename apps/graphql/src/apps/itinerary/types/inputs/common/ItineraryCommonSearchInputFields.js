9b822816e8113ae76899b54255a4672216af1636
albertalises <albert.alises@gmail.com>
2019-05-10 10:41:32 +0200

    Update dotenv-plugin and set basePath

--- apps/graphql/src/apps/itinerary/types/inputs/common/ItineraryCommonSearchInputFields.js
+++ apps/graphql/src/apps/itinerary/types/inputs/common/ItineraryCommonSearchInputFields.js
@@ -16,8 +16,7 @@ export default {
     type: SortSearchInput,
   },
   limit: {
-    description:
-      'Limit of the number of results to get from the search (maximum 200)',
+    description: 'Limit of results to get from the search (maximum 200)',
     type: GraphQLInt,
   },
   passengers: {
