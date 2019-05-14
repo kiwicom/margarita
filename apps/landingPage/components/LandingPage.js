17dd791b85b07bd6a4b1667d37696ef8a5af9017
KarinaDavtyan <k.davtyan27@gmail.com>
2019-05-14 08:08:11 +0200

    Update header of the landing page (#768)
    Summary: plane image set as a background for both mobile and desktop. Content height "inside" the image is calculated and set in order to place Logos correctly below it.

--- apps/landingPage/components/LandingPage.js
+++ apps/landingPage/components/LandingPage.js
@@ -6,14 +6,12 @@ import Header from './Header';
 import Footer from './Footer';
 import Demo from './Demo';
 import Info from './Info';
-import Headline from './Headline';
 import Playground from './Playground';
 
 export default function LandingPage() {
   return (
     <>
       <Header />
-      <Headline />
       <Playground />
       <Info />
       <Demo />
