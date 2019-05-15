52e388c9910221413c438d5b20b8a5cf9957a1b9
Jan Václavík <jvaclavik@gmail.com>
2019-05-15 14:08:30 +0200

    Styling menu in Landing page

--- apps/landingPage/components/AccentedText.js
+++ apps/landingPage/components/AccentedText.js
@@ -3,7 +3,7 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  font-size: 30px;
+  font-size: 27px;
   font-weight: 700;
   line-height: 50px;
 `;
---
+++ apps/landingPage/components/Title.js
@@ -0,0 +1,12 @@
+// @flow
+
+import styled from 'styled-components';
+import { defaultTokens } from '@kiwicom/orbit-design-tokens';
+
+export default styled.div`
+  font-size: 40px;
+  color: ${defaultTokens.paletteInkDark};
+  display: inline-block;
+  padding: 50px 0 30px 0;
+  font-weight: bold;
+`;
