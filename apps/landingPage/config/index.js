c9bdb3f10e7e0f3a3ace89babdc64e410b156a0c
Jan Václavík <jvaclavik@seznam.cz>
2019-05-20 17:21:34 +0200

    Style multiplatform section in landing page (#793)

---
+++ apps/landingPage/config/index.js
@@ -0,0 +1,16 @@
+// @flow
+
+export default {
+  phoneWidth: 280,
+  phoneHeight: 500,
+  phoneTopMargin: 100,
+  phoneWidthBuffer: 400,
+  featureHeight: 300,
+  featuresMarginBottom: 120,
+  sharedLinkProps: {
+    activeClass: 'active',
+    spy: true,
+    smooth: true,
+    duration: 500,
+  },
+};
--- apps/landingPage/containers/Features.js
+++ apps/landingPage/containers/Features.js
@@ -1,48 +1,172 @@
 // @flow
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import { defaultTokens } from '@kiwicom/orbit-design-tokens';
+import { Button } from '@kiwicom/orbit-components/lib/';
+import { Link } from 'react-scroll';
 
 import Content from '../components/Content';
 import Highlight from '../components/Highlight';
 import AccentedText from '../components/AccentedText';
 import Title from '../components/Title';
+import FloatedPhone from '../components/FloatedPhone';
+import Config from '../config';
 
-export default function Info() {
-  return (
-    <Content id="features">
-      <Title>Features</Title>
-      <Feature>
-        <AccentedText>
-          <Highlight>Search for flights.</Highlight> Get an access to the
-          database of 15 billion connections virtually interlined by Kiwi.com
-        </AccentedText>
-      </Feature>
-      <Feature>
-        <AccentedText>
-          <Highlight>Booking.</Highlight> Use our powerful infrastructure to
-          book the trips and gain your commission - the best one in the
-          industry.
-        </AccentedText>
-      </Feature>
-      <Feature>
-        <AccentedText>
-          <Highlight>Manage my booking</Highlight> allows your customers to
-          modify the trip, edit passenger’s details or purchase additional
-          services.
-        </AccentedText>
-      </Feature>
-      <Feature>
-        <AccentedText>
-          <Highlight>Payments</Highlight> allow your users to pay instantly for
-          their bookings, right within your app.
-        </AccentedText>
-      </Feature>
-    </Content>
-  );
+type Props = {|
+  scroll: number,
+|};
+
+export default class Features extends React.Component<Props> {
+  componentDidUpdate() {
+    const featuresPosition = this.findPosition(this.featuresRef);
+    if (featuresPosition) {
+      const newFeaturesTopPosition = featuresPosition.top + this.props.scroll;
+      if (this.featuresTopPosition !== newFeaturesTopPosition) {
+        this.featuresTopPosition = newFeaturesTopPosition;
+        this.featuresHeight = featuresPosition.height;
+      }
+    }
+  }
+
+  featuresHeight: number = 0;
+  featuresTopPosition: number = 0;
+  featuresRef: ?Element = null;
+
+  findPosition = (ref: ?Element) => {
+    const DOMNode = ReactDOM.findDOMNode(ref);
+    if (DOMNode && DOMNode instanceof Element)
+      return DOMNode.getBoundingClientRect();
+    return null;
+  };
+
+  setRef = (ref: ?Element) => {
+    this.featuresRef = ref;
+  };
+
+  render() {
+    return (
+      <Content id="multiplatform" ref={this.setRef}>
+        <MultiplatformWrapper>
+          <PhotoWrapper>
+            <Photo
+              src="/static/android.png"
+              srcSet={`/static/android.png 1x,
+            /static/android@2x.png 2x, /static/android@3x.png 3x`}
+              alt="Android"
+            />
+            <Photo
+              src="/static/mac.png"
+              srcSet={`/static/mac.png 1x,
+            /static/mac@2x.png 2x, /static/mac@3x.png 3x`}
+              alt="Mac"
+            />
+          </PhotoWrapper>
+          <MultiplatformText>
+            <AccentedText big>
+              <Highlight>Multi-platform app</Highlight> for all major platforms
+              — web, iOS and Android.
+            </AccentedText>
+          </MultiplatformText>
+
+          <LinkWithoutStyle
+            href="#features"
+            to="features"
+            {...Config.sharedLinkProps}
+          >
+            <Button circled>See App features</Button>
+          </LinkWithoutStyle>
+        </MultiplatformWrapper>
+        <FeaturesContainer id="features">
+          <Title>App features</Title>
+          <Feature>
+            <AccentedText big>
+              <Highlight>Search for flights.</Highlight> Get an access to the
+              database of 15 billion connections virtually interlined by
+              Kiwi.com
+            </AccentedText>
+          </Feature>
+          <Feature>
+            <AccentedText big>
+              <Highlight>Booking.</Highlight> Use our powerful infrastructure to
+              book the trips and gain your commission - the best one in the
+              industry.
+            </AccentedText>
+          </Feature>
+          <Feature>
+            <AccentedText big>
+              <Highlight>Manage my booking</Highlight> allows your customers to
+              modify the trip, edit passenger’s details or purchase additional
+              services.
+            </AccentedText>
+          </Feature>
+          <Feature>
+            <AccentedText big>
+              <Highlight>Payments</Highlight> allow your users to pay instantly
+              for their bookings, right within your app.
+            </AccentedText>
+          </Feature>
+        </FeaturesContainer>
+
+        <FloatedPhone
+          scroll={this.props.scroll}
+          featuresHeight={this.featuresHeight}
+          featuresTopPosition={this.featuresTopPosition}
+        />
+      </Content>
+    );
+  }
 }
 
+const MultiplatformWrapper = styled.div`
+  padding-top: 100px;
+  @media (min-width: ${defaultTokens.widthBreakpointDesktop}px) {
+    padding-right: ${Config.phoneWidth}px;
+    scroll-snap-align: start;
+    scroll-snap-type: y proximity;
+  }
+`;
+const MultiplatformText = styled.div`
+  margin-top: 60px;
+  margin-bottom: 20px;
+`;
+const FeaturesContainer = styled.div`
+  @media (min-width: ${defaultTokens.widthBreakpointDesktop}px) {
+    margin-top: 80px;
+    margin-bottom: ${Config.featuresMarginBottom}px;
+  }
+`;
 const Feature = styled.div`
   display: flex;
-  margin-bottom: 50px;
+  padding: 20px 0;
+  align-items: center;
+  @media (min-width: ${defaultTokens.widthBreakpointDesktop}px) {
+    padding: 0 ${Config.phoneWidthBuffer}px 0 0;
+    height: ${Config.featureHeight}px;
+  }
+`;
+
+const PhotoWrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  transition: all 0.05s linear;
+  position: relative;
+`;
+const Photo = styled.img`
+  display: inline-block;
+  height: 200px;
+  padding-right: 50px;
+  @media (min-width: ${defaultTokens.widthBreakpointLargeMobile}px) {
+    height: 300px;
+  }
+  @media (min-width: ${defaultTokens.widthBreakpointTablet}px) {
+    height: 400px;
+  }
+  @media (min-width: ${defaultTokens.widthBreakpointDesktop}px) {
+    height: 500px;
+  }
+`;
+const LinkWithoutStyle = styled(Link)`
+  text-decoration: none;
 `;
